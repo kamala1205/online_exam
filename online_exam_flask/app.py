ccdc34e4860c5e954d0eff60b5ec246888ad6299
kamala1205 <studyverse0512@gmail.com>
2026-06-25 02:28:46 +0530

    Update app.py

--- online_exam_flask/app.py
+++ online_exam_flask/app.py
@@ -761,4 +761,11 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
